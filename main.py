baf7c439c6fe9b04492ecdfd5e57b5b5b1d2aad0
Tri Duong <danieldng52@gmail.com>
2023-10-14 13:09:52 +0700

    change to read .env variables

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI
 import requests
 import json
 from fastapi.middleware.cors import CORSMiddleware
+from config import settings
+
 
 app = FastAPI()
 origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:8088",
+    # "http://localhost",
+    # "http://localhost:8080",
+    # "http://localhost:8088",
+    # settings.SUPERSET_BASE_URL,
     '*'
 ]
 
@@ -20,32 +23,32 @@ app.add_middleware(
 )
 
 
-@app.get("/guest_login")
-async def guest_login():
-    url = "http://127.0.0.1:8088/api/v1/security/login"
-    payload = json.dumps({"password": "admin", "provider": "db", "refresh": "true", "username": "admin"})
+@app.get("/guest_login/{dashboard_id}")
+async def guest_login(dashboard_id):
+    url = f"{settings.SUPERSET_BASE_URL}/api/v1/security/login"
+    payload = json.dumps({"password": settings.SUPERSET_PW, "username": settings.SUPERSET_USER,
+                          "provider": "db", "refresh": "true"})
     headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 
-    responsel = requests.request("POST", url, headers=headers, data=payload)
-    print(responsel.text)
-    superset_access_token = json.loads(responsel.text)['access_token']
+    _response = requests.post(url, headers=headers, data=payload)
+    superset_access_token = json.loads(_response.text)['access_token']
     payload = json.dumps({
         "user": {
-            "username": "admin",
+            "username": settings.SUPERSET_USER,
         },
         "resources": [{
             "type": "dashboard",
-            "id": "f6596a6c-f176-40c7-b159-4ec483a010ce"
+            "id": dashboard_id  # e.g. "f6596a6c-f176-40c7-b159-4ec483a010ce"
         }],
         "rls": []
     })
 
     bearer_token = "Bearer " + superset_access_token
-    print(bearer_token)
+    # print(bearer_token)
     response2 = requests.post(
-        "http://127.0.0.1:8088/api/v1/security/guest_token",
+        url=f"{settings.SUPERSET_BASE_URL}/api/v1/security/guest_token",
         data=payload,
         headers={"Authorization": bearer_token, 'Accept': 'application/json', 'Content-Type': 'application/json'})
-    print(response2.json())
+    # print(response2.json())
     return response2.json()['token']
 
